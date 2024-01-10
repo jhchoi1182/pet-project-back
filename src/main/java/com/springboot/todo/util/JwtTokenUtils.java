59960f1e7928366847214a293cd3671bae93e9e7
jhchoi1182 <jhchoi1182@gmail.com>
2024-01-11 06:01:34 +0900

    feat: Jwt 토큰 생성

---
+++ src/main/java/com/springboot/todo/util/JwtTokenUtils.java
@@ -0,0 +1,34 @@
+package com.springboot.todo.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenUtils {
+
+    public static String generateToken(String username, String encodedPassword, String key, long expiredTimeMs) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("password", encodedPassword);
+        claims.put("username", username);
+
+        long nowMillis = System.currentTimeMillis();
+
+        return Jwts.builder()
+                .claims(claims)
+                .issuedAt(new Date(nowMillis))
+                .expiration(new Date(nowMillis + expiredTimeMs))
+                .signWith(getKey(key))
+                .compact();
+    }
+
+    private static Key getKey(String key) {
+        byte[] keyBytes = key.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
